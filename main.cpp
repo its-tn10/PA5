32bac3a6e479b52b9b0aaf602e56a5c2e7c2d712
Tien Tavu <its.tn10@tamu.edu>
2019-11-27 22:34:27 -0600

    Final report, and updated main to reflect word doc

--- main.cpp
+++ main.cpp
@@ -13,33 +13,27 @@
 using namespace std;
 
 int main() {
-    DoubleHashTable table;
-
-    ifstream ist{"dictionary.txt"};
+    string fileName;
+    cout << "Enter the dictionary file: ";
+    cin >> fileName;
+    ifstream ist{fileName};
 
     if (!ist)   throw runtime_error("cant open input file");
+
+    ChainingHashTable chainTable;
+    ProbingHashTable probingTable;
+    DoubleHashTable doubleHashTable;
+
     string word;
-
-    chrono::time_point<chrono::system_clock> start, end;
-    start = chrono::system_clock::now();
-
-    for (int i = 0; i < 50000; i++) {
-    //while (ist >> word) 
-        ist >> word;
-        table.insert(word, 1);
+    while (ist >> word) {
+        chainTable.insert(word, 1);
+        probingTable.insert(word, 1);
+        doubleHashTable.insert(word, 1);
     }
-    end = chrono::system_clock::now();
-
-   
-    std::chrono::duration<double> elapsed_seconds = end - start; 
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end); 
-    cout << elapsed_seconds.count() << endl;
-
-    cout << table.get("astronaut") << endl;
-    cout << table.remove("astronaut") << endl;
-    cout << table.get("astronaut") << endl;
-
-    table.printAll("output.txt");
+    
+    chainTable.printAll("Chaining.txt");
+    probingTable.printAll("Probing.txt");
+    doubleHashTable.printAll("DoubleHashing.txt");
 
     return 0;
 }
